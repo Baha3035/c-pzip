3f324ad0cac7dc1fc84a867e5761dea88e475e0a
bakhtiyar <batmaaanpls@gmail.com>
2025-08-28 16:20:28 +0600

    enabling storage

--- day_3/ordered_data_chunks_zip.c
+++ day_3/ordered_data_chunks_zip.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 
 typedef struct {
     char* data;
     int chunk_number;
+    char** results;
 } chunk_info_t;
 
 void* compress_chunk(void* arg) {
@@ -16,15 +17,22 @@ void* compress_chunk(void* arg) {
 
     sleep(info->chunk_number == 0 ? 1 : 2);
 
+    char* result = malloc(20);
+    sprintf(result, "a3b3");
+
+    info->results[info->chunk_number] = result;
+
     printf("Chunk %d compressed!\n", info->chunk_number);
     return NULL;
 }
 
 int main() {
+    char* results[2] = {NULL, NULL};
+
     pthread_t threads[2];
     chunk_info_t chunks[] = {
-        {"aaabbb", 0},
-        {"cccddd", 1}
+        {"aaabbb", 0, results},
+        {"cccddd", 1, results}
     };
 
     for(int i = 0; i < 2; i++) {
@@ -33,6 +41,11 @@ int main() {
 
     for(int i = 0; i < 2; i++) {
         pthread_join(threads[i], NULL);
+    }
+
+    for(int i = 0; i < 2; i++) {
+        printf("%s\n", results[i]);
+        free(results[i]);
     }
 
     // It is to know which result belongs to which chunk
