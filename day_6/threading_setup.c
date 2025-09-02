817d40075cee0ea827348ce48d7d22250c542394
bakhtiyar <batmaaanpls@gmail.com>
2025-09-02 17:10:41 +0600

    add rle functionality and merge previous logic

---
+++ day_6/threading_setup.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+// Build a thread coordinator
+typedef struct {
+    int thread_id;
+    char* file_data;
+    int start;
+    int end;
+    char* result;
+    int result_length;
+} ThreadData;
+
+void* compression_work(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
+
+    int char_count = 0;
+    char current_character = data->file_data[data->start];
+    int pos = 0;
+
+    for(int i = data->start; i < data->end; i++) {
+        if (current_character == data->file_data[i]) {
+            char_count++;
+        } else {
+            data->result[pos++] = current_character;
+            *((int*)&data->result[pos]) = char_count;
+            pos += sizeof(int);
+
+            current_character = data->file_data[i];
+            char_count = 1;
+        }
+    }
+
+    data->result[pos++] = current_character;
+    *((int*)&data->result[pos]) = char_count;
+    pos += sizeof(int);
+
+    data->result[pos] = '\0';
+    data->result_length = pos;
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    // TODO: Read entire file into memory
+    
+    char* filename = argv[1];
+    FILE* fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("Error opening file!");
+        return -1;
+    }
+
+    // Seek to end of file
+    fseek(fp, 0, SEEK_END);
+
+    // Get current position (which is the file size)
+    long file_size = ftell(fp);
+
+    // Add this after getting file size to go back to beginning
+    fseek(fp, 0, SEEK_SET);
+
+    char* file = malloc(file_size);
+    ssize_t bytes_read = fread(file, 1, file_size, fp);
+    if (bytes_read > 0) {
+        file[bytes_read] = '\0';
+    }
+
+    // TODO: Calculate chunk sizes
+    int num_threads = 4;
+    int chunk_size = file_size / num_threads;
+    // TODO: Create worker threads
+    ThreadData thread_data[4];
+    pthread_t threads[4];
+
+    for(int i = 0; i < num_threads; i++) {
+        thread_data[i].thread_id = i;
+        thread_data[i].start = i * chunk_size;
+        thread_data[i].end = (i == num_threads - 1) ? file_size : (i + 1) * chunk_size;
+        thread_data[i].file_data = file;
+        thread_data[i].result = malloc(2 * file_size);
+
+        pthread_create(&threads[i], NULL, compression_work, &thread_data[i]);
+    }
+
+    // TODO: Wait for all workers
+
+    // TODO: Merge results
+    
+    fclose(fp);
+    free(file);
+    return 0;
+}
